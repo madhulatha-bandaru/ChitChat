1398dad18d26839b04487f00aea25f209acba18c
Madhu Latha Bandaru <151339301+madhulatha-bandaru@users.noreply.github.com>
2025-07-07 19:54:57 +0530

    Update db.js

--- server/src/lib/db.js
+++ server/src/lib/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`MongoDB connected : ${conn.connection.host}`)
+    console.log("Connected to the Database successfully")
   } catch(error) {
     console.log("Error during Database connection : " + error);
   }
